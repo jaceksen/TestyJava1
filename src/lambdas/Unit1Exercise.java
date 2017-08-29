4909bbde2428e67847703fa26fd9fccae5881b83
jaceksen <jaceksen@gmail.com>
2017-08-29 07:39:05 +0200

    170828

---
+++ src/lambdas/Unit1Exercise.java
@@ -0,0 +1,26 @@
+package lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Unit1Exercise {
+
+	public static void main(String[] args) {
+		
+		List<Person> people = Arrays.asList(
+				new Person("Jacek","Seń",45),
+				new Person("Rima","Avetyan",32),
+				new Person("Maja","Seń",5),
+				new Person("Tigran","Seń",3),
+				new Person("Stanisław","Seń",2),
+				new Person("Kamil","Seń",17)
+				);
+		
+		//sorting by last name
+		
+		//metoda do wydrukowania wszystkiego w liście
+		
+		//wydrukowanie wszystkich którch nazwisko rozpoczyna się do S
+	}
+
+}
